0224726655e3a4e18bfb0a89febdcfc1a2d7514b
nelliewarin <wane22bh@student.ju.se>
2025-03-05 13:59:13 +0100

    url hyperlinks

--- public/app.js
+++ public/app.js
@@ -11,7 +11,19 @@ fetch("http://localhost:3000/api/stores")
     data.forEach((shop) => {
       let newElem = document.createElement("li");
       newElem.classList.add("listItem");
-      newElem.innerText = `${shop.name}`;
+
+      if (shop.url != null) {
+        let url;
+        if (shop.url.startsWith("http")) {
+          url = shop.url;
+        } else {
+          url = `https://${shop.url}`;
+        }
+        newElem.innerHTML = `<a href='${url}'>${shop.name}</a>`;
+      } else {
+        newElem.innerText = `${shop.name}`;
+      }
+
       list.appendChild(newElem);
     });
   });
--- server.js
+++ server.js
@@ -9,7 +9,7 @@ connectDB();
 //index.html (public folder) is displayed at http://localhost:3000
 app.use("/", express.static("public"));
 
-// Connect to stores.json 
+// Connect to stores.json
 // app.get("/api/stores", (req, res) => {
 //   res.sendFile(path.join(__dirname, "public", "stores.json"));
 // });
@@ -17,7 +17,7 @@ app.use("/", express.static("public"));
 app.get("/api/stores", async (req, res) => {
   try {
     const dbres = await client.query(
-      "SELECT data->>'name' AS name FROM stores;"
+      "SELECT data->>'name' AS name, data->>'url' AS url FROM stores;"
     );
     console.log("All stores:", dbres.rows);
     res.json(dbres.rows);
